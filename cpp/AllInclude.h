c6cdec7fb8736f1b6206b2f7150fc65f6e5651ee
Ricky Liu <ctrickyliu@mail.scut.edu.cn>
2023-03-04 10:57:02 +0800

    配置线下调试环境

--- cpp/AllInclude.h
+++ cpp/AllInclude.h
@@ -25,12 +25,20 @@
 #include <vector>
 using namespace std;
 
+const int null_node = 1853189228;
+
 struct ListNode {
     int val;
     ListNode* next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode* next) : val(x), next(next) {}
+    ~ListNode() {
+        if (next) {
+            delete next;
+        }
+        cout << "Delete " << val << " node.\n";
+    }
 };
 
 struct TreeNode {
@@ -40,6 +48,15 @@ struct TreeNode {
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    ~TreeNode() {
+        if (left) {
+            delete left;
+        }
+
+        if (right) {
+            delete right;
+        }
+    }
 };
 
 template <typename T>
@@ -48,7 +65,7 @@ std::ostream& operator<<(std::ostream& out, const std::vector<T>& v) {
     if (!v.empty()) {
         out << '[';
         std::copy(v.begin(), v.end(), std::ostream_iterator<T>(out, ", "));
-        out << "]";
+        out << "\b\b]";
     }
     // out << "]";
     return out;
@@ -60,7 +77,7 @@ std::ostream& operator<<(std::ostream& out, const std::set<T>& v) {
     if (!v.empty()) {
         out << '(';
         std::copy(v.begin(), v.end(), std::ostream_iterator<T>(out, ", "));
-        out << ")";
+        out << "\b\b)";
     }
     // out << "]";
     return out;
@@ -68,13 +85,14 @@ std::ostream& operator<<(std::ostream& out, const std::set<T>& v) {
 
 template <typename T>
 std::ostream& operator<<(std::ostream& out, const std::vector<vector<T> >& vv) {
-    out << '[';  // vector< vector<T> >
+    out << "[\n";  // vector< vector<T> >
     if (!vv.empty()) {
         for (auto v : vv) {
-            out << '[';
+            out << "  [";
             std::copy(v.begin(), v.end(), std::ostream_iterator<T>(out, ", "));
-            out << "], ";
+            out << "\b\b],\n";
         }
+        out << "\b\b";
     }
     out << "]";
     return out;
@@ -86,8 +104,57 @@ std::ostream& operator<<(std::ostream& out, const std::map<T1, T2>& v) {
     for (auto& item : v) {
         out << item.first << ": " << item.second << ", ";
     }
-    out << "}";
+    out << "\b\b}";
     return out;
+}
+
+ostream& operator<<(ostream& os, ListNode* lp) {
+    ListNode* cur = lp;
+    while (cur != nullptr) {
+        os << cur->val << " -> ";
+        cur = cur->next;
+    }
+    os << "null";
+    return os;
+}
+
+ostream& operator<<(ostream& os, TreeNode* root) {
+    queue<TreeNode*> q;
+    q.push(root);
+    os << "[";
+
+    while (!q.empty()) {
+        TreeNode* cur = q.front();
+        q.pop();
+        if (cur) {
+            os << cur->val << ", ";
+            if (cur->left == cur->right && cur->left == nullptr) {
+                continue;
+            }
+            q.push(cur->left);
+            q.push(cur->right);
+        } else {
+            os << "null, ";
+        }
+    }
+
+    os << "\b\b]";
+    return os;
+}
+
+TreeNode* construct_binary_tree(vector<int>& vec, int len, int i) {
+    if (vec.empty() || len < 1)
+        return nullptr;
+    TreeNode* root = nullptr;
+    if (i < len && vec[i] != null_node) {
+        root = new TreeNode(0);
+        if (root == nullptr)
+            return nullptr;
+        root->val = vec[i];
+        root->left = construct_binary_tree(vec, len, 2 * i + 1);
+        root->right = construct_binary_tree(vec, len, 2 * i + 2);
+    }
+    return root;
 }
 
 #endif
