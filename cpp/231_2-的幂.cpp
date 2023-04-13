4a317625205b599454e4318a8ca1fa1f037f127f
Ricky Liu <ctrickyliu@mail.scut.edu.cn>
2023-04-13 22:47:41 +0800

    重整笔记分类

--- cpp/231_2-的幂.cpp
+++ cpp/231_2-的幂.cpp
@@ -7,6 +7,12 @@
 #include "AllInclude.h"
 
 // @lc code=start
+/**
+ * Accepted 2023-04-13
+ * 1108/1108 cases passed (0 ms)
+ * Your runtime beats 100 % of cpp submissions
+ * Your memory usage beats 70.78 % of cpp submissions (5.7 MB)
+ */
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
