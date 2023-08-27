f853aee2fb7cc83e98c859c3124b5c7fe71cf98a
Ricky Liu <ctrickyliu@mail.scut.edu.cn>
2023-08-27 21:08:18 +0800

    20230927 add code (PC)

---
+++ cpp/213_打家劫舍-ii.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=213 lang=cpp
+ *
+ * [213] 打家劫舍 II
+ */
+
+#include "AllInclude.h"
+
+// @lc code=start
+class Solution {
+    public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) {
+            return nums[0];
+        } else if (n == 2) {
+            return max(nums[0], nums[1]);
+        }
+        return max(dp_fun(nums, 0, n - 2), dp_fun(nums, 1, n - 1));
+    }
+
+private:
+    int dp_fun(const vector<int>& nums, int start, int end) {
+        int first = nums[start], second = max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = second;
+            second = max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+};
+
+// @lc code=end
+
